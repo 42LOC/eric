963e9949b8404d0fd4d6c7298e44b79796535037
VIKHEAD <nebopidzemlejy@gmail.com>
2021-03-19 14:15:10 +0200

    Task 5.6.7.9

--- product_analyzer/models/models.py
+++ product_analyzer/models/models.py
@@ -3,24 +3,28 @@
 from odoo import models, fields, api
 import datetime
 from dateutil import relativedelta
-import random
 
 
 class ProductAnalyzer(models.Model):
     _name = 'product_analyzer'
     _description = 'product_analyzer'
+    _rec_name = 'field'
 
-    # default_code = fields.Char(size=64, required=True, index=True)
-    # default_code = fields.Integer('Internal Code', readonly=True,
-    # compute='code',
-    # default=random.randint(1000, 9000)
-    # )
-    categ_id = fields.Many2one('product.category', string='Product Category'
-                               # related='product_id.categ_id'
-                               )
+    field = fields.Char(default='Configuration', store=False)
+    name = fields.Char(string="Name", compute='_get_compute_name')
+    available = fields.Boolean(string="Available", default=True)
+    categ_id = fields.Many2one('product.category', string='Product Category')
+    create_date = fields.Date(string="Start day", default=datetime.date.today())
     line_ids = fields.One2many('product_analyzer.sheet', 'sheet_id')
     start_date = fields.Date(string='Order history start date',
-                             default=datetime.date.today() - relativedelta.relativedelta(months=1))
+                             default=datetime.date.today() -
+                                     relativedelta.relativedelta(months=1))
+
+    @api.depends('start_date')
+    def _get_compute_name(self):
+        for line in self:
+            line.name = f'{line.start_date.strftime("%m/%d/%Y")} ' \
+                        f'- {line.create_date.strftime("%m/%d/%Y")}'
 
 
 class ProductAnalyzerSheet(models.Model):
@@ -32,25 +36,23 @@ class ProductAnalyzerSheet(models.Model):
     product_id = fields.Many2one('product.product', string='Product',
                                  domain="[('categ_id', '=', categ_id)]")
     sku = fields.Char(string='SKU', related='product_id.barcode')
-    title = fields.Char(string='Title', related='product_id.product_tmpl_id.name')
-    direct = fields.Float(string='Direct', related='product_id.stock_move_ids.product_uom_qty')
-    replenishment = fields.Float(string='Replenishment')
+    title = fields.Char(string='Title',
+                        related='product_id.product_tmpl_id.name')
+    direct = fields.Float(string='Direct',
+                          related='product_id.stock_move_ids.product_uom_qty')
+    inbound = fields.Float(string='Inbound')
     sold = fields.Float(string='Qty Sold', compute='_compute_production')
-    inventory = fields.Float(string='Inventory', related='product_id.qty_available')
-    demand = fields.Float(string='Demand')
-    production = fields.Float(string='Production', compute='_compute_production')
+    inventory = fields.Float(string='Inventory',
+                             related='product_id.qty_available')
+    send_in = fields.Float(string='Send In')
+    production = fields.Float(string='Production',
+                              compute='_compute_production')
     actual_demand = fields.Float(string='Actual Demand')
-    actual_production = fields.Float(string='Actual Production')
+    actual_cut = fields.Float(string='Actual Cut')
     completed = fields.Date(string='Completed')
 
-    @api.depends('inventory', 'demand')
+    @api.depends('inventory', 'send_in')
     def _compute_production(self):
         for record in self:
-            record.production = record.demand - record.inventory
-            record.sold = record.direct + record.replenishment
-
-
-    # @api.depends('direct', 'replenishment')
-    # def _compute_sold(self):
-    #     for record in self:
-
+            record.production = record.send_in - record.inventory
+            record.sold = record.direct + record.inbound
